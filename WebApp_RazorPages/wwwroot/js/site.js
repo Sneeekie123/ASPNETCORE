b3605bd0d1aa6a6f7c4cc79bca1b4189e7dce6d7
Sneeekie123 <143397337+Sneeekie123@users.noreply.github.com>
2024-03-18 18:29:50 +0100

    JS ändrar så det fungerar

--- WebApp_RazorPages/wwwroot/js/site.js
+++ WebApp_RazorPages/wwwroot/js/site.js
@@ -1,1 +1,6 @@
-﻿
+﻿const btn = document.querySelector('#mobile-menu-btn');
+
+btn.addEventListener('click', () => {
+    const menu = document.querySelector('#mobile-menu')
+    menu.ariaExpanded = !JSON.parse(menu.ariaExpanded)
+});
