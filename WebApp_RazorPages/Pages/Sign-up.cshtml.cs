8b7a8ee93709124ee3a4daed61cfb95a4febc96e
Sneeekie123 <143397337+Sneeekie123@users.noreply.github.com>
2024-03-19 22:33:49 +0100

    Form hantering

--- WebApp_RazorPages/Pages/Sign-up.cshtml.cs
+++ WebApp_RazorPages/Pages/Sign-up.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp_RazorPages.Models;
 
-namespace WebApp_RazorPages.Pages
+namespace WebApp_RazorPages.Pages;
+
+public class Sign_upModel : PageModel
 {
-    public class Sign_upModel : PageModel
+
+    public SignUpFormModel Form { get; set; } = null!;
+
+
+
+    public void OnGet()
     {
-        public void OnGet()
-        {
-        }
+        Form = new SignUpFormModel();
+    }
+
+    public void OnPost()
+    {
+
     }
 }
