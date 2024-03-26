01e294d705a66611694507723f8049f1b81f0775
Sneeekie123 <143397337+Sneeekie123@users.noreply.github.com>
2024-03-26 21:55:35 +0100

    Lagt till JS cod för validering utav formulär.

--- WebApp_RazorPages/Models/SignUpFormModel.cs
+++ WebApp_RazorPages/Models/SignUpFormModel.cs
@@ -1,31 +1,38 @@
 ﻿
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
+using WebApp_RazorPages.Helpers;
 
 namespace WebApp_RazorPages.Models;
 
 public class SignUpFormModel
 {
+    [DataType(DataType.Text)]
     [Display(Name = "First name" , Prompt = "Enter your first name")]
     [Required(ErrorMessage = "Invalid first name")]
+    [MinLength(2, ErrorMessage = "Enter your first name")]
     public string FirstName { get; set; } = null!;
 
 
-
+    [DataType(DataType.Text)]
     [Display(Name = "Last name", Prompt = "Enter your Last name")]
     [Required(ErrorMessage = "Invalid last name")]
+    [MinLength(2, ErrorMessage = "Enter your last name")]
     public string LastName { get; set; } = null!;
 
 
     [DataType(DataType.EmailAddress)]
     [Display(Name = "Email", Prompt = "Enter your email address")]
     [Required(ErrorMessage = "Invalid email address")]
+    [RegularExpression(@"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$", ErrorMessage = "Invalid email address")]
     public string Email { get; set; } = null!;
+
 
 
     [DataType(DataType.Password)]
     [Display(Name = "Password", Prompt = "Enter your password")]
     [Required(ErrorMessage = "Invalid password")]
+    [RegularExpression(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]", ErrorMessage = "Invalid password")]
     public string Password { get; set; } = null!;
 
 
@@ -38,31 +45,10 @@ public class SignUpFormModel
 
 
     [Display(Name = "I agree to the Terms & Conditions")]
-    [CheckBoxRequired(ErrorMessage = "You must agree to the terms & conditions")]
+    [CheckBoxRequired(ErrorMessage = "You must agree to the terms & conditions.")]
     public bool Terms { get; set; } = false;
 
 
 }
 
-public class CheckBoxRequired : ValidationAttribute, IClientModelValidator
-{
 
-    public override bool IsValid(object value)
-    {
-        if (value is bool)
-        {
-            return (bool)value;
-        }
-
-        return false;
-    }
-
-    public void AddValidation(ClientModelValidationContext context)
-    {
-
-        // skicka iväg formuläret till en userService för registrering
-        // var result = _userService.CreateUser(From);
-
-        context.Attributes.Add("data-val-checkboxrequired", FormatErrorMessage(context.ModelMetadata.GetDisplayName()));
-    }
-}
